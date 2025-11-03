35774288d53bff8ac5ab44670743164357e228d5
divine <divinemensah102@gmail.com>
2025-11-02 19:26:26 -0500

    update!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { ReactNode } from "react";
 import { Providers } from "./providers";
 import Header from "@/components/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 export const metadata: Metadata = {
   title: "TSender",
@@ -14,6 +16,7 @@ export default function RootLayout(props: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <SpeedInsights />
         <Providers>
           <Header />
           {props.children}
